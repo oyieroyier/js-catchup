61b8fe7acafd9099c3a4e395e7417033bbbae3c6
Bob Oyier <boboyier92@gmail.com>
2024-07-04 13:35:58 +0300

    Starter Code

---
+++ script/app.js
@@ -0,0 +1,34 @@
+const menuLinks = ['Home', 'About', 'Contact', 'Blog', 'Careers'];
+
+const socialMediaLinks = [
+	{
+		linkName: 'Facebook',
+		linkUrl: 'https://facebook.com',
+		icon: `<i class="fa-brands fa-facebook"></i>`,
+	},
+	{
+		linkName: 'Twitter',
+		linkUrl: 'https://twitter.com',
+		icon: `<i class="fa-brands fa-twitter"></i>`,
+	},
+	{
+		linkName: 'Instagram',
+		linkUrl: 'https://instagram.com',
+		icon: `<i class="fa-brands fa-instagram"></i>`,
+	},
+	{
+		linkName: 'GitHub',
+		linkUrl: 'https://github.com',
+		icon: `<i class="fa-brands fa-github"></i>`,
+	},
+	{
+		linkName: 'LinkedIn',
+		linkUrl: 'https://linkedin.com',
+		icon: `<i class="fa-brands fa-linkedin"></i>`,
+	},
+	{
+		linkName: 'Dribbble',
+		linkUrl: 'https://dribbble.com',
+		icon: `<i class="fa-brands fa-dribbble"></i>`,
+	},
+];
---
+++ script/users.js
@@ -0,0 +1,33 @@
+const users = [
+	{
+		name: 'John',
+		age: 30,
+		occupation: 'Software Engineer',
+		hobbies: ['reading', 'hiking', 'coding'],
+		image: 'https://placeholder.com/100x100',
+	},
+	{
+		name: 'Jane',
+		age: 25,
+		occupation: 'Data Scientist',
+		hobbies: ['swimming', 'traveling', 'cooking'],
+		image: 'https://placeholder.com/100x100',
+	},
+
+	{
+		name: 'Bob',
+		age: 35,
+		occupation: 'Manager',
+		hobbies: ['fishing', 'hiking', 'coding'],
+		image: 'https://placeholder.com/100x100',
+	},
+
+	{
+		name: 'Alice',
+		age: 40,
+		occupation: 'Designer',
+		hobbies: ['reading', 'hiking', 'painting'],
+		image: 'https://placeholder.com/100x100',
+	},
+];
+
