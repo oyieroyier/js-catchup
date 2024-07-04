2614fbdffc3d8fb010c935b51172fb2d82dabeeb
Bob Oyier <boboyier92@gmail.com>
2024-07-04 13:39:19 +0300

    Starter Code

--- script/users.js
+++ script/users.js
@@ -1,13 +1,19 @@
 const users = [
 	{
-		name: 'John',
+		name: {
+			firstName: 'Kamau',
+			lastName: 'Muchangi',
+		},
 		age: 30,
 		occupation: 'Software Engineer',
 		hobbies: ['reading', 'hiking', 'coding'],
 		image: 'https://placeholder.com/100x100',
 	},
 	{
-		name: 'Jane',
+		name: {
+			firstName: 'William',
+			lastName: 'Omondi',
+		},
 		age: 25,
 		occupation: 'Data Scientist',
 		hobbies: ['swimming', 'traveling', 'cooking'],
@@ -15,7 +21,10 @@ const users = [
 	},
 
 	{
-		name: 'Bob',
+		name: {
+			firstName: 'Kabi',
+			lastName: 'Wa Judas',
+		},
 		age: 35,
 		occupation: 'Manager',
 		hobbies: ['fishing', 'hiking', 'coding'],
@@ -23,11 +32,13 @@ const users = [
 	},
 
 	{
-		name: 'Alice',
+		name: {
+			firstName: 'Alice',
+			lastName: 'Makori',
+		},
 		age: 40,
 		occupation: 'Designer',
 		hobbies: ['reading', 'hiking', 'painting'],
 		image: 'https://placeholder.com/100x100',
 	},
 ];
-
